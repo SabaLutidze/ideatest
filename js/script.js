e686bc6f37e615a2c73f505b0488886cbd897e6d
sabalutidze <saba.luto@gmail.com>
2022-03-15 02:39:52 +0400

    initial

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$('.slider').slick({
+    nextArrow: '.next-arr',
+    prevArrow: '.prev-arr'
+});
+
+$('.team-img').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
